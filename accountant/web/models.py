cc0819bced5202fc563760d8302355e54dac636d
aliasghar <aliasghar.salimi.05@gmail.com>
2024-04-22 03:35:57 +0330

    Expense added to models'

--- accountant/web/models.py
+++ accountant/web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=250)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
