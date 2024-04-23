7ac6763123909cf21ee0ca68722ec7f1f3b3a199
aliasghar <aliasghar.salimi.05@gmail.com>
2024-04-23 15:04:09 +0330

    Income added to models and some butifications

--- accountant/web/models.py
+++ accountant/web/models.py
@@ -9,4 +9,13 @@ class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.text
+        return "\"{}\" {}|{}".format(self.text, self.date, self.amount)
+    
+class Income(models.Model):
+    text = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "\"{}\" {}|{}".format(self.text, self.date, self.amount)
