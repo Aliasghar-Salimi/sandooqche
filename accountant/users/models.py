8bb9cd3f1567b45523028fcfb060dbb22c837175
aliasghar <aliasghar.salimi.05@gmail.com>
2024-05-16 00:51:18 +0330

    passwordresetcode and profile class deleted from users models

--- accountant/users/models.py
+++ accountant/users/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-
-
-# Extending User Model Using a One-To-One Link
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username 
-
 
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,14 +7,4 @@ class Token(models.Model):
 
     def __str__(self):
         return "{}-token".format(self.user)
-    
-class PasswordResetCode(models.Model):
-    code = models.CharField(max_length=32)
-    email = models.CharField(max_length=120)
-    time = models.DateTimeField()
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)  
-    
-    def __str__(self):
-        return f"Reset code for {self.username}"
     
