ba6aac01d88467cdb0577b87231b8409ddb4f90b
aliasghar <aliasghar.salimi.05@gmail.com>
2024-05-16 01:06:32 +0330

    new views added to web views.py, as query_expenses and query_incomes

--- accountant/web/urls.py
+++ accountant/web/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from web import views
 
+app_name = 'web'
 urlpatterns = [
     path('submit/expense', views.submit_expense, name='submit_expense'),
     path('submit/income', views.submit_income, name='submit_income'),
+    path('query/expenses/', views.query_expenses, name='query_expenses'),
+    path('query/incomes/', views.query_incomes, name='query_incomes'),
+    path('generalstat/', views.generalstat, name='generalstat'),
 ]
--- accountant/web/views.py
+++ accountant/web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from json import JSONEncoder
 from django.views.decorators.csrf import csrf_exempt
@@ -6,11 +6,14 @@ from django.views.decorators.cache import cache_page
 from web.models import User, Expense, Income
 from time import gmtime, strftime
 
+from django.views.decorators.http import require_POST
+
+from web.models import Expense, Income
+from django.core import serializers
+
+from django.db.models import Count, Sum
 
 # Create your views here.
-
-
-
 
 @csrf_exempt
 @cache_page(60 * 15)
@@ -40,4 +43,48 @@ def submit_income(request):
                            user=this_user, date=date)
     
     print(request.POST)
+    
     return JsonResponse({'status':'working'}, encoder=JSONEncoder)
+
+# return General Status of a user as Json (income,expense)
+
+@csrf_exempt
+@require_POST
+def query_expenses(request):
+    this_token = request.POST['token']
+    num = request.POST.get('num', 10)
+    this_user = get_object_or_404(User, token__token=this_token)
+    expenses = Expense.objects.filter(user=this_user).order_by('-date')[:num]
+    expenses_serialized = serializers.serialize("json", expenses)
+
+    return JsonResponse(expenses_serialized, encoder=JSONEncoder, safe=False)
+
+@csrf_exempt
+@require_POST
+def query_incomes(request):
+    this_token = request.POST['token']
+    num = request.POST.get('num', 10)
+    this_user = get_object_or_404(User, token__token=this_token)
+    expenses = Income.objects.filter(user=this_user).order_by('-date')[:num]
+    expenses_serialized = serializers.serialize("json", expenses)
+
+    return JsonResponse(expenses_serialized, encoder=JSONEncoder, safe=False)
+
+
+@csrf_exempt
+@require_POST
+def generalstat(request):
+    # TODO: should get a valid duration (from - to), if not, use 1 month
+    # TODO: is the token valid?
+    this_token = request.POST['token']
+    this_user = get_object_or_404(User, token__token=this_token)
+    income = Income.objects.filter(user=this_user).aggregate(
+        Count('amount'), Sum('amount'))
+    expense = Expense.objects.filter(user=this_user).aggregate(
+        Count('amount'), Sum('amount'))
+    context = {}
+    context['expense'] = expense
+    context['income'] = income
+
+    return JsonResponse(context, encoder=JSONEncoder)
+
